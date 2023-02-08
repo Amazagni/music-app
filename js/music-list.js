9083bfb6bf0a45b22244b7c187e1e272be163a37
Amazagni <marcinxkomputer@gmail.com>
2023-02-08 17:18:12 +0100

    html css done

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "In the End",
+        artist: "Linkin Park",
+        img: "Music-1",
+        src: "Music-1"
+    },
+    {
+        name: "Take It From Me",
+        artist: "KONGOS",
+        img: "Music-2",
+        src: "Music-2"
+    },
+    {
+        name: "Light",
+        artist: "Nathan Wagner",
+        img: "Music-3",
+        src: "Music-3"
+    },
+    {
+        name: "The Phoenix",
+        artist: "Fall Out Boy",
+        img: "Music-4",
+        src: "Music-4"
+    },
+    {
+        name: "Nails in the Coffin",
+        artist: "Jared James Nichols",
+        img: "Music-5",
+        src: "Music-5"
+    }
+]
